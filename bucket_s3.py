a62cfd9cce8e91b9f0750959f150ce3b0ecb7966
sarthak-magar15 <66588272+sarthak-magar15@users.noreply.github.com>
2025-03-17 04:09:05 -0400

    Update bucket_s3.py

--- bucket_s3.py
+++ bucket_s3.py
@@ -23,3 +23,15 @@ def pull_data_from_mongo(query: Dict):
 def push_mongo_to_s3(query):
     for element in pull_data_from_mongo(query):
         upload(element)
+
+def aws_upload(data: Dict):
+    database = aws_lib.connect("AKIAF6BAFJKR45SAWSZ5", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkja")
+    database.push(data)
+
+def aws_upload(data: Dict):
+    database = aws_lib.connect("AKIAF6BAFJKR45SAWSZ5", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkja")
+    database.push(data)
+
+def aws_upload(data: Dict):
+    database = aws_lib.connect("AKIAF6BAFJKR45SAWSZ5", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkja")
+    database.push(data)
