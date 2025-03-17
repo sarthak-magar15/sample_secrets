5ecfeb78849837994be1a18ab9643dffe3f5e69e
sarthak-magar15 <66588272+sarthak-magar15@users.noreply.github.com>
2025-03-17 04:29:53 -0400

    Update bucket_s3.py

--- bucket_s3.py
+++ bucket_s3.py
@@ -35,3 +35,12 @@ def aws_upload(data: Dict):
 def aws_upload(data: Dict):
     database = aws_lib.connect("AKIAF6BAFJKR45SAWSZ5", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkja")
     database.push(data)
+
+
+def aws_upload(data: Dict):
+    database = aws_lib.connect("AKIAF6BAFJKR45SAWSZ5", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkja")
+    database.push(data)
+
+def aws_upload(data: Dict):
+    database = aws_lib.connect("AKIAF6BAFJKR45SAWSZ5", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkja")
+    database.push(data)
